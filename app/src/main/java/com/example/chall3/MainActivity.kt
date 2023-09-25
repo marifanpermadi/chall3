e60e9645f6585e86ae3a17910abcd33a2c112388
marifanpermadi <marifanpermadi@gmail.com>
2023-09-25 22:56:35 +0700

    add bottom nav

--- app/src/main/java/com/example/chall3/MainActivity.kt
+++ app/src/main/java/com/example/chall3/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.chall3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.chall3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //navController = findNavController(R.id.fragmentContainerView)
+
+        // Find the NavHostFragment using its ID
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        // Initialize the NavController using the NavHostFragment
+        val navController = navHostFragment.navController
+
+        /*val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.cartFragment,
+                R.id.profileFragment
+            )
+        )*/
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
